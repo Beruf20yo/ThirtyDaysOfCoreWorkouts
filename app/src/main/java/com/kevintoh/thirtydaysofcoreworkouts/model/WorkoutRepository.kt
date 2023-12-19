c207d742f5f4520c338d3973c4069c5cdb749119
RinatG <rinat_galimov_1992@mail.ru>
2023-12-20 00:42:16 +0300

    Initial commit

---
+++ app/src/main/java/com/kevintoh/thirtydaysofcoreworkouts/model/WorkoutRepository.kt
@@ -0,0 +1,189 @@
+package com.kevintoh.thirtydaysofcoreworkouts.model
+
+import com.kevintoh.thirtydaysofcoreworkouts.R
+
+object WorkoutRepository {
+    val workouts = listOf(
+        Workout(
+            nameRes = R.string.bird_dog,
+            instructionRes = R.string.bird_dog_instructions,
+            imageRes = R.drawable.day_1,
+            generalInformation = "Это единственные 2 планеты в нашей Солнечной системе, которые не имеют лун"
+        ),
+        Workout(
+            nameRes = R.string.bear_plank,
+            instructionRes = R.string.bear_plank_instructions,
+            imageRes = R.drawable.day_2,
+            generalInformation = "Если звезда, которая проходит слишком близко к черной дыре, она может быть разорвана"
+        ),
+        Workout(
+            nameRes = R.string.plank_shoulder_tap,
+            instructionRes = R.string.plank_shoulder_tap_instructions,
+            imageRes = R.drawable.day_3,
+            generalInformation = "Самая горячая планета в нашей Солнечной системе-Венера.\n"
+        ),
+        Workout(
+            nameRes = R.string.mountain_climbers,
+            instructionRes = R.string.mountain_climbers_instructions,
+            imageRes = R.drawable.day_4,
+            generalInformation = "Нашей Солнечной системе 4,57 миллиарда лет"
+        ),
+        Workout(
+            nameRes = R.string.leg_raises,
+            instructionRes = R.string.leg_raises_instructions,
+            imageRes = R.drawable.day_5,
+            generalInformation = "Энцелад, один из малых спутников Сатурна, отражает 90% солнечного света"
+        ),
+        Workout(
+            nameRes = R.string.v_hold,
+            instructionRes = R.string.v_hold_instructions,
+            imageRes = R.drawable.day_6,
+            generalInformation = "Самая высокая обнаруженная гора - это гора Олимп, расположенная на Марсе"
+        ),
+        Workout(
+            nameRes = R.string.toe_touches,
+            instructionRes = R.string.toe_touches_instructions,
+            imageRes = R.drawable.day_7,
+            generalInformation = "Вихревая Галактика (М51) была первым небесным объектом, идентифицированным как спиральный"
+        ),
+        Workout(
+            nameRes = R.string.plank_up_downs,
+            instructionRes = R.string.plank_up_downs_instructions,
+            imageRes = R.drawable.day_8,
+            generalInformation = "Световой год - это расстояние, пройденное светом за один год"
+        ),
+        Workout(
+            nameRes = R.string.dead_bug,
+            instructionRes = R.string.dead_bug_instructions,
+            imageRes = R.drawable.day_9,
+            generalInformation = "Галактика Млечный Путь имеет ширину 105 700 световых лет"
+        ),
+        Workout(
+            nameRes = R.string.scissors,
+            instructionRes = R.string.scissors_instructions,
+            imageRes = R.drawable.day_10,
+            generalInformation = "Солнце весит примерно в 330 000 раз больше Земли"
+        ),
+        Workout(
+            nameRes = R.string.russian_twists,
+            instructionRes = R.string.russian_twists_instructions,
+            imageRes = R.drawable.day_11,
+            generalInformation = "Следы, оставленные на Луне, не исчезнут, так как нет ветра"
+        ),
+        Workout(
+            nameRes = R.string.side_plank,
+            instructionRes = R.string.side_plank_instructions,
+            imageRes = R.drawable.day_12,
+            generalInformation = "Из-за меньшей силы тяжести человек, который весит 99 кг на Земле, будет весить 38,1 кг на Марсе"
+        ),
+        Workout(
+            nameRes = R.string.glute_bridge,
+            instructionRes = R.string.glute_bridge_instructions,
+            imageRes = R.drawable.day_13,
+            generalInformation = "Известно 79 спутников, вращающихся вокруг Юпитера"
+        ),
+        Workout(
+            nameRes = R.string.spiderman_crunches,
+            instructionRes = R.string.spiderman_crunches_instructions,
+            imageRes = R.drawable.day_14,
+            generalInformation = "Марсианские сутки длятся 24 часа 39 минут 35 секунд"
+        ),
+        Workout(
+            nameRes = R.string.heel_touches,
+            instructionRes = R.string.heel_touches_instructions,
+            imageRes = R.drawable.day_15,
+            generalInformation = "Спутник наблюдения и зондирования кратеров НАСА (LCROSS) обнаружил свидетельства наличия воды на Луне"
+        ),
+        Workout(
+            nameRes = R.string.windshield_wipers,
+            instructionRes = R.string.windshield_wipers_instructions,
+            imageRes = R.drawable.day_16,
+            generalInformation = "Солнце совершает полный оборот один раз в 25-35 дней"
+        ),
+        Workout(
+            nameRes = R.string.reverse_crunches,
+            instructionRes = R.string.reverse_crunches_instructions,
+            imageRes = R.drawable.day_17,
+            generalInformation = "Земля - единственная планета, не названная в честь бога "
+        ),
+        Workout(
+            nameRes = R.string.criss_cross_flutter_kicks,
+            instructionRes = R.string.criss_cross_flutter_kicks_instructions,
+            imageRes = R.drawable.day_18,
+            generalInformation = " Из-за гравитационного притяжения Солнца и Луны у нас есть приливы и отливы"
+        ),
+        Workout(
+            nameRes = R.string.superman,
+            instructionRes = R.string.superman_instructions,
+            imageRes = R.drawable.day_19,
+            generalInformation = "Плутон меньше, чем Соединенные Штаты"
+        ),
+        Workout(
+            nameRes = R.string.burpees,
+            instructionRes = R.string.burpees_instructions,
+            imageRes = R.drawable.day_20,
+            generalInformation = "Согласно математике, белые дыры возможны, хотя до сих пор мы их не нашли"
+        ),
+        Workout(
+            nameRes = R.string.high_knees,
+            instructionRes = R.string.high_knees_instructions,
+            imageRes = R.drawable.day_21,
+            generalInformation = "На Венере больше вулканов, чем на любой другой планете нашей Солнечной системы"
+        ),
+        Workout(
+            nameRes = R.string.sliding_pike,
+            instructionRes = R.string.sliding_pike_instructions,
+            imageRes = R.drawable.day_22,
+            generalInformation = "Голубое свечение урана обусловлено наличием газов в его атмосфере"
+        ),
+        Workout(
+            nameRes = R.string.seated_knee_tucks,
+            instructionRes = R.string.seated_knee_tucks_instructions,
+            imageRes = R.drawable.day_23,
+            generalInformation = "В нашей Солнечной системе это 4 планеты, известные как газовые гиганты: Юпитер, Сатурн, Уран и Нептун"
+        ),
+        Workout(
+            nameRes = R.string.high_plank,
+            instructionRes = R.string.high_knees_instructions,
+            imageRes = R.drawable.day_24,
+            generalInformation = "Уран имеет 27 лун, которые были открыты"
+        ),
+        Workout(
+            nameRes = R.string.standing_criss_cross_crunch,
+            instructionRes = R.string.standing_criss_cross_crunch_instructions,
+            imageRes = R.drawable.day_25,
+            generalInformation = "Из-за его уникального наклона сезон на Уране эквивалентен 21 земному году"
+        ),
+        Workout(
+            nameRes = R.string.wood_chop,
+            instructionRes = R.string.wood_chop_instructions,
+            imageRes = R.drawable.day_26,
+            generalInformation = "Спутник Нептуна, Тритон, вращается вокруг планеты в обратном направлении"
+        ),
+        Workout(
+            nameRes = R.string.hollow_body_hold,
+            instructionRes = R.string.hollow_body_hold_instructions,
+            imageRes = R.drawable.day_27,
+            generalInformation = "Тритон постепенно приближается к планете, вокруг которой вращается"
+        ),
+        Workout(
+            nameRes = R.string.rolling_plank,
+            instructionRes = R.string.rolling_plank_instructions,
+            imageRes = R.drawable.day_28,
+            generalInformation = "В космосе звезд больше, чем песчинок в мире"
+        ),
+        Workout(
+            nameRes = R.string.breakdance,
+            instructionRes = R.string.breakdance_instructions,
+            imageRes = R.drawable.day_29,
+            generalInformation = "Нептуну требуется почти 165 земных лет, чтобы сделать одну орбиту Солнца"
+        ),
+        Workout(
+            nameRes = R.string.seagull,
+            instructionRes = R.string.seagull_instructions,
+            imageRes = R.drawable.day_30,
+            generalInformation = "Самая большая луна Плутона, Харон, составляет половину размера Плутона"
+        )
+
+    );
+}
